23edb23345162abe8675ad83e4504443ba207b27
Daniel Pavone <danielpavone@gmail.com>
2023-12-04 13:08:32 -0300

    feat: configure layout

---
+++ frontend/src/styles/themes/default.ts
@@ -0,0 +1,10 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  'gray-300': '#C4C4CC',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  'green-500': '#00875F',
+  'green-700': '#015F43',
+}
