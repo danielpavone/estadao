7ce83ecf013b1b3657ebb04915f674f38ce0a7a2
Daniel Pavone <danielpavone@gmail.com>
2023-12-04 13:11:42 -0300

    feat: create newsService

---
+++ frontend/src/services/newsService.ts
@@ -0,0 +1,22 @@
+const NEWS_API_URL = 'http://localhost:3000'
+
+export async function getNews() {
+  try {
+    const response = await fetch(`${NEWS_API_URL}/news`)
+    const data = await response.json()
+    console.log(data);
+    return data
+  } catch (error) {
+    return []
+  }
+}
+
+export async function getNewsById(id: string) {
+  try {
+    const response = await fetch(`${NEWS_API_URL}/news/${id}`)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    return {}
+  }
+}
